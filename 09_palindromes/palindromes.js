cc7ae2234590e8589c2b01ec903d551e4bb04445
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2024-01-12 14:22:18 +0600

    09 exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    result = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return result === result.split("").reverse().join("");
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
