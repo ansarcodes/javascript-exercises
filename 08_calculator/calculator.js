7737fea70b848a4847f4be4815c5601f49c18102
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2024-01-12 13:22:00 +0600

    08 exercises solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (...nums) {
+  return nums.reduce((sum, current) => sum + current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (num1 = 0, num2 = 0) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  for (let i = 1; i<=num; i++) {
+    result = result * i;
+  }
+  return result;
+
+
 };
 
 // Do not edit below this line
